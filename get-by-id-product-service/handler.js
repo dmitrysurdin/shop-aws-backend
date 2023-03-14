2d78f75bd3abd2ce289646eb8bc44453f38765f9
Dmitry Surdin <dimasurdin@yandex.ru>
2023-03-14 15:26:44 +0400

    refactoring

--- get-by-id-product-service/handler.js
+++ get-by-id-product-service/handler.js
@@ -6,29 +6,14 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.getProductById = async (event) => {
     const { productId } = event.pathParameters;
-    const products = await dynamodb.scan({ TableName: "products", }).promise();
-    const stocks = await dynamodb.scan({ TableName: "stocks", }).promise();
-    const product = products.Items.find((p) => p.id === productId);
-    const stock = stocks.Items.find((p) => p.product_id === productId);
-    const result = { ...product, count: stock.count };
 
     try {
-        if (!products || !stocks) {
-            return {
-                statusCode: 404,
-                body: JSON.stringify({ message: "Products are not found in DB" })
-            };
-        }
-    } catch (error) {
-        console.error(error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ message: "An error occurred" })
-        };
-    }
+        const products = await dynamodb.scan({ TableName: "products", }).promise();
+        const stocks = await dynamodb.scan({ TableName: "stocks", }).promise();
+        const product = products.Items.find((p) => p.id === productId);
+        const stock = stocks.Items.find((p) => p.product_id === productId);
+        const result = { ...product, count: stock.count };
 
-
-    try {
         if (!product) {
             return {
                 statusCode: 404,
