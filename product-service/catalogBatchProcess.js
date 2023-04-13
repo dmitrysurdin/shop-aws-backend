1872de4d02576fdc94d938827a65d668fb9d77f9
Dmitry Surdin <dimasurdin@yandex.ru>
2023-04-14 00:30:06 +0400

    change url

--- product-service/catalogBatchProcess.js
+++ product-service/catalogBatchProcess.js
@@ -4,6 +4,9 @@ const sns = new AWS.SNS();
 exports.handler = async (event) => {
     const products = event.Records.map((record) => JSON.parse(record.body));
 
+    // Create products in the database
+    await Promise.all(products.map((product) => createProduct(product)));
+
     // Publish a message to the SNS topic
     const message = `Created ${products.length} products`;
     const params = {
